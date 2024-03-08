f9b763fcfcaf29e04260bb69dd532c52745a1bca
Brayan Esteves <besteves@moveapps.cl>
2024-03-08 17:03:47 -0400

    Structure Base -  Layout Child and Content [08/03/2024]

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,10 @@ export default function RootLayout({
       <body className={`${montserrat.className}`}>
         {children}
       </body>
+
+      <footer className='py-10 flex justify-center items-center'>
+        Footer example.
+      </footer>
     </html>
   );
 }
